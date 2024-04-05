66c6f2db695a910b6626622eb68979469fdce33f
Seema-spec <seemaariyan158@gmail.com>
2024-04-05 10:43:57 +0530

    new changes

--- src/component/Deposit.jsx
+++ src/component/Deposit.jsx
@@ -25,18 +25,19 @@ const Deposit = () => {
     setError('')
     setMessage('Succesfully Deposit Money')
   };
-  const handleBack = () =>{
+  
+  const handleBack = () => {
     navigate(-1)
   }
 
-  const handleChange = (e) =>{
+  const handleChange = (e) => {
     setAmount(e.target.value);
     setMessage('');
   }
 
   return (
     <div className="container">
-      <div className='medium-box'><Header/></div>
+      <div className='medium-box'><Header /></div>
       <div class="d-flex justify-content-center">
         <div class="card p-3">
           <div class="d-flex flex-row justify-content-between text-align-center">
@@ -46,10 +47,10 @@ const Deposit = () => {
         </div>
         <div className="card-notes">
           <div className='inner-card-notes'>
-          <label>
-            Denomination
-          </label>
-          <select value={denomination} onChange={(e) => setDenomination(e.target.value)} >
+            <label>
+              Denomination
+            </label>
+            <select value={denomination} onChange={(e) => setDenomination(e.target.value)} >
               <option value="100">Rs. 100</option>
               <option value="500">Rs. 500</option>
               <option value="1000">Rs. 1000</option>
@@ -57,21 +58,21 @@ const Deposit = () => {
             </select>
           </div>
           <div className='inner-card-notes'>
-          <label>
-            Number of Notes
-          </label>
-          <input
-            type="number"
-            value={amount}
-            onChange={(e) => handleChange(e)}
-            placeholder="Enter deposit amount"
-            className='card-input'
-          />
+            <label>
+              Number of Notes
+            </label>
+            <input
+              type="number"
+              value={amount}
+              onChange={(e) => handleChange(e)}
+              placeholder="Enter deposit amount"
+              className='card-input'
+            />
           </div>
         </div>
         <div className='button-class'>
-        <button onClick={handleBack} className='card-button-back'>Back</button>
-        <button onClick={handleDeposit} className='card-button'>Deposit</button>
+          <button onClick={handleBack} className='card-button-back'>Back</button>
+          <button onClick={handleDeposit} className='card-button'>Deposit</button>
         </div>
         {error ? <p style={{ color: 'red' }}>{error}</p> : <p style={{ color: 'red' }}>{message}</p>}
       </div>
